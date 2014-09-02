78ec5ea9d46e59989096ca1c2491165a8f0328ca
Jelle Versele <jelle@versele.be>
2014-09-02 23:39:52 +0200

    WIP.

---
+++ app/index.js
@@ -0,0 +1,64 @@
+'use strict';
+var util = require('util');
+var path = require('path');
+var yeoman = require('yeoman-generator');
+var yosay = require('yosay');
+
+var PintxosGenerator = yeoman.generators.Base.extend({
+  initializing: function () {
+    this.pkg = require('../package.json');
+  },
+
+  prompting: function () {
+    var done = this.async();
+
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the astonishing Pintxos generator!'
+    ));
+
+    var prompts = [{
+      type: 'input',
+      name: 'componentName',
+      message: 'What is the name of the Pintxos component you are about to start building?',
+      default: this.appname
+    }, {
+      type: 'input',
+      name: 'author',
+      message: 'Could you tell me the author\'s name?'
+    }, {
+      type: 'input',
+      name: 'email',
+      message: 'Can I also get his or her e-mail address?'
+    }];
+
+    this.prompt(prompts, function (props) {
+      
+      this.tplData = {
+        componentName: props.componentName,
+        packageName: 'pintxos-' + props.componentName.toLowerCase(),
+        author: props.author,
+        email: props.email
+      };
+      
+      done();
+
+    }.bind(this));
+  },
+
+  writing: {
+    app: function () {
+      this.dest.mkdir('test');
+      console.log(this.tplData)
+      this.template('_README.md', 'README.md', this.tplData);
+      this.template('_bower.json', 'bower.json', this.tplData);
+      this.template('_package.json', 'package.json', this.tplData);
+    },
+  },
+
+  end: function () {
+    this.installDependencies();
+  }
+});
+
+module.exports = PintxosGenerator;
